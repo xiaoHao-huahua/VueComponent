9535384529c2ee20f6da6736f05edbace0df3554
imperfect <wenghaolula@outlook.com>
2019-12-07 22:30:43 +0800

    12345

---
+++ custom/event-bus.js
@@ -0,0 +1,87 @@
+/* 
+自定义全局事件总线对象模块
+*/
+
+// (function(window) {
+//   let eventBus = {};
+//   /*
+// {
+//   add:[f1,f2,f3],
+//   delete:[f1]
+
+//   eventName:[]
+// }
+// 'add',
+
+// (data) => {
+//       console.log('add1()', data)
+//     }
+//  */
+//   eventBus.on = function(eventName, listener) {
+//     let callbacks = eventBus[eventName];
+//     if (!callbacks) {
+//       callbacks = [];
+//       eventBus[eventName] = callbacks;
+//     }
+//     eventBus[eventName].push(listener);
+//   };
+
+//   eventBus.emit = function(eventName, data) {
+//     if(eventBus[eventName]){
+//     eventBus[eventName].forEach(callback=>{
+//       callback(data)
+//     })
+//     }
+//   };
+
+//   eventBus.off = function(eventName) {
+//     if(eventName === undefined){
+//       eventBus = {}
+//     }else{
+//       delete eventBus[eventName]
+//     }
+//   }
+
+//   window.eventBus = eventBus;
+// })(window);
+
+// (function(window) {
+//   let eventBus = {};
+//   /*
+// // {
+// //   add:[f1,f2,f3],
+// //   delete:[f1]
+
+// //   eventName:[]
+// // }
+// //  */
+
+//   eventBus.on = function(eventName, listener) {
+//     let callbacks = eventBus[eventName];
+//     if (!callbacks) {
+//       eventBus[eventName] = [];
+//     }
+//     eventBus[eventName].push(listener);
+//   };
+
+//   eventBus.emit = function(eventName, data) {
+//     if (eventBus[eventName]) {
+//       eventBus[eventName].forEach((callback)=>{
+//         callback(data)
+//       })
+//     }
+//   };
+//   eventBus.off = function(eventName) {
+//     if (eventName === undefined) {
+//       eventBus[eventName] = {};
+//     } else {
+//       delete eventBus[eventName];
+//     }
+//   };
+
+//   window.eventBus = eventBus;
+// })(window);
+
+
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,22 @@
 <template>
-  <div class="todo-container">
-    <div class="todo-wrap">
-      <Header :addTodo="addTodo" />
-      <List :todos="todos" :deleteTodos="deleteTodos" :upDateTodos="upDateTodos" />
-      <Footer :todos="todos" :isCheckAll="isCheckAll" :deleteChecked='deleteChecked' />
-    </div>
+    <div class="container">
+    <Search/>
+    <Users/>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import Header from "./components/Header";
-import List from "./components/List";
-import Footer from "./components/Footer";
-export default {
-  data() {
-    return {
-      todos: [
-        { id: 1, title: "打篮球", completed: true },
-        { id: 2, title: "乒乓球", completed: false },
-        { id: 3, title: "羽毛球", completed: false }
-      ]
-    };
-  },
-  components: {
-    Header,
-    List,
-    Footer
-  },
-  methods: {
-    addTodo(value) {
-      this.todos.unshift({ id: Date.now(), title: value, completed: false });
-    },
-    deleteTodos(index) {
-      this.todos.shift(index, 1);
-    },
-    upDateTodos(todo, value) {
-      todo.completed = value;
-    },
-    isCheckAll(isChecked) {
-      this.todos.forEach(todo => {
-        todo.completed = isChecked;
-      });
-    },
-    deleteChecked() {
-      this.todos = this.todos.filter(todo=>{
-        return todo.completed === false
-      })
+import Search from './components/search'
+import Users from './components/users'
+  export default {
+    components:{
+      Search,
+      Users
     }
   }
-};
 </script>
 
 <style scoped>
-/*app*/
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
+
+
 </style>
